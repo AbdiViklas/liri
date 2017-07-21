ec460ed37fa9127387d63961621f4fc60a296ce7
Andy Bonner <abonner@duke.edu>
2017-07-21 16:08:18 -0400

    define doWhat()

--- liri.js
+++ liri.js
@@ -6,22 +6,26 @@ var twitterKeys = keys.twitterKeys;
 var command = process.argv[2];
 var input = process.argv[3];
 
-switch (command) {
-  case "my-tweets":
-    twitter();
-    break;
-  case "spotify-this-song":
-    spotify(input);
-    break;
-  case "movie-this":
-    omdb(input);
-    break;
-  case "do-what-it-says":
-    doWhat();
-    break;
-  default:
-    console.log("I'm sorry, I don't recognize that command. Possible commands are:\n  'my-tweets'\n  'spotify-this-song'\n  'movie-this' \n  'do-what-it-says'");
-    break;
+assignAction(command, input);
+
+function assignAction(command, input) {
+  switch (command) {
+    case "my-tweets":
+      twitter();
+      break;
+    case "spotify-this-song":
+      spotify(input);
+      break;
+    case "movie-this":
+      omdb(input);
+      break;
+    case "do-what-it-says":
+      doWhat();
+      break;
+    default:
+      console.log("I'm sorry, I don't recognize that command. Possible commands are:\n  'my-tweets'\n  'spotify-this-song'\n  'movie-this' \n  'do-what-it-says'");
+      break;
+  }
 }
 
 function twitter() {
@@ -37,5 +41,11 @@ function omdb(searchTerm) {
 }
 
 function doWhat() {
-  
+  fs.readFile("random.txt", "utf8", function(err, data){
+    if (err) {
+      console.log("Error:", err);
+    }
+    var dataArr = data.split(",");
+    assignAction(dataArr[0], dataArr[1]);
+  })
 }

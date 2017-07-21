f8d3b9bf135c72ec74b6ee4e3fbf19ad54e357fd
Andy Bonner <abonner@duke.edu>
2017-07-21 09:58:10 -0400

    import twitter keys

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('keys.js is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'rTUjlSCbpimhzQwsQMhQs9968',
+  consumer_secret: 'pCiQmjnPTg0EjweWH6TuditNP0o0T0oUvuzl6MHk9arZjevhzM',
+  access_token_key: '888073911132459012-sznr1dNEx5Kj9LAYMfbBZrbcP2ObHSq',
+  access_token_secret: 'qjPvo3UasnWb2pnHKm8auW4WlUgKliYdYLlXrT03JKqaf',
+}
